d8bd4a0844329abc6b092f1c2abde12a9cd1f2ae
JackWang071 <35980971+JackWang071@users.noreply.github.com>
2019-10-22 20:16:59 -0700

    Add files via upload

---
+++ Lab-8/Cs542_a8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs542_a8;
+
+/**
+ *
+ * @author Jack and Duran
+ */
+public class Cs542_a8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
