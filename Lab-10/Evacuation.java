c47b6112820eeec24e99daa535c8e355658fc8e9
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-11-05 23:24:00 -0800

    Update Evacuation.java

--- Lab-10/Evacuation.java
+++ Lab-10/Evacuation.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_a10;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,11 +9,42 @@ package cs542_a10;
  */
 public class Evacuation extends Decision {
     
+    private List<Hazard_Report_IF> sub;
+    private List<Hazard_Report_IF> subM;
+    private List<Hazard_Report_IF> subL;
+    
     public Evacuation(int p){
-        super("Evacuation", p);
+        super(p);
     }
     
     public void execute(CEO ceo){
+        sub = ceo.getSubordinates();
+        int i;
+        
+        //evac reg employees and sort the others into groups
+        for(i = 0; i < sub.size(); i++) {
+            if( sub.get(i) instanceof Manager){
+                subM.add(sub.get(i));
+            }
+            if( sub.get(i) instanceof Leader){
+                subL.add(sub.get(i));
+            }
+            if( sub.get(i) instanceof RegularEmployee){
+                sub.get(i).evacuate();
+            }
+        }
+        //evacutate leaders (supervisors/projectLeads)
+        for(i = 0; i < subL.size(); i++) {
+            subL.get(i).evacuate();
+        }
+        
+        //evacutate managers
+        for(i = 0; i < subM.size(); i++) {
+            subM.get(i).evacuate();
+        }       
+        
+        //evactuate CEO
         ceo.evacuate();
     } 
+    
 }
