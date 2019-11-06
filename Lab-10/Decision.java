d4afcf2635e3c6bee33518ff9705392be8350d11
JackWang071 <35980971+JackWang071@users.noreply.github.com>
2019-11-05 21:49:56 -0800

    Jack version of Decision/Hazard classes

--- Lab-10/Decision.java
+++ Lab-10/Decision.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cs542_a10;
 
 /**
@@ -11,11 +16,15 @@ abstract class Decision {
     private int priority;
     
     public Decision(String t, int hL, int p){
+        this.type = t;
+        this.hazard_lvl = hL;
+        this.priority = p;
     }
     
     public int getPriority() {
+        return priority;
     }
     
-    public void execute(ceo CEO){
+    public void execute(CEO ceo){
     }
 }
--- Lab-10/FileReport.java
+++ Lab-10/FileReport.java
@@ -9,17 +9,22 @@ package cs542_a10;
  *
  * @author Jack and Duran
  */
-public class Evacuation extends Decision {
+public class FileReport extends Decision  {
     
     private String type;
     private int hazard_lvl;
     private int priority;
-    
-    public Evacuation(String t, int hL, int p){
+        
+    public FileReport(String t, int hL, int p){
         super(t, hL, p);
     }
     
+    public int getHazard_lvl() {
+        return hazard_lvl;
+    }
+    
     public void execute(CEO ceo){
-        System.out.println("CEO " + ceo.getName() + " has a report filed with the city's environmental department.");
+        System.out.println("I want a report on this incident on my desk in the next 2 minutes");
     } 
+    
 }
