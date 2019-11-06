d7bd4e438096017da88d75e7d681adac3260ae91
JackWang071 <35980971+JackWang071@users.noreply.github.com>
2019-11-05 23:59:48 -0800

    Add files via upload

--- Lab-10/Evacuation.java
+++ Lab-10/Evacuation.java
@@ -1,5 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cs542_a10;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,42 +13,55 @@ import java.util.ArrayList;
  */
 public class Evacuation extends Decision {
     
-    private List<Hazard_Report_IF> sub;
-    private List<Hazard_Report_IF> subM;
-    private List<Hazard_Report_IF> subL;
-    
     public Evacuation(int p){
-        super(p);
+        super("Evacuation", p);
     }
     
     public void execute(CEO ceo){
-        sub = ceo.getSubordinates();
-        int i;
+        List<Hazard_Report_IF> ceo_subordinates = ceo.getSubordinates();
+        List<Hazard_Report_IF> managers = new ArrayList();        
+        List<Hazard_Report_IF> leaders = new ArrayList();
         
-        //evac reg employees and sort the others into groups
-        for(i = 0; i < sub.size(); i++) {
-            if( sub.get(i) instanceof Manager){
-                subM.add(sub.get(i));
+        System.out.println("REGULAR EMPLOYEES evacuate FIRST.");
+        //Iterating over managers
+        for(Hazard_Report_IF manager : ceo_subordinates){
+            if(manager instanceof RegularEmployee){
+                manager.evacuate();
             }
-            if( sub.get(i) instanceof Leader){
-                subL.add(sub.get(i));
-            }
-            if( sub.get(i) instanceof RegularEmployee){
-                sub.get(i).evacuate();
+            else{
+                Admin confirmed_manager = (Admin) manager;
+                List<Hazard_Report_IF> manager_sub = confirmed_manager.getSubordinates();
+                managers.add(manager);
+                //Iterating over leaders
+                for(Hazard_Report_IF leader : manager_sub){
+                    if(leader instanceof RegularEmployee){
+                        leader.evacuate();
+                    }
+                    else{
+                        Admin confirmed_leader = (Admin) leader;
+                        List<Hazard_Report_IF> leader_sub = confirmed_leader.getSubordinates();
+                        leaders.add(leader);
+                        //Iterating over Regular Employees?
+                        for(Hazard_Report_IF employee : leader_sub){
+                            employee.evacuate();
+                        }
+                    }
+                }
             }
         }
+        
+        System.out.println("SUPERVISORS and PROJECT LEADS evacuate SECOND.");
         //evacutate leaders (supervisors/projectLeads)
-        for(i = 0; i < subL.size(); i++) {
-            subL.get(i).evacuate();
+        for(Hazard_Report_IF sub1 : leaders){
+            sub1.evacuate();
         }
-        
-        //evacutate managers
-        for(i = 0; i < subM.size(); i++) {
-            subM.get(i).evacuate();
-        }       
-        
+        System.out.println("MANAGERS evacuate THIRD.");
+        //evacutate leaders (supervisors/projectLeads)
+        for(Hazard_Report_IF sub1 : managers){
+            sub1.evacuate();
+        }
         //evactuate CEO
+        System.out.println("The CEO evacuates LAST.");
         ceo.evacuate();
     } 
-    
 }
