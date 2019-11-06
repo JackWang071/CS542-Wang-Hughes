5f6cf4e332db134182630d18f08252422df8ea28
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-11-05 21:36:00 -0800

    Update Decision.java

--- Lab-10/Decision.java
+++ Lab-10/Decision.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cs542_a10;
+package Lab10;
 
 /**
  *
- * @author Jack
+ * @author Jack and Duran
  */
-public class Decision {
+abstract class Decision {
     
+    private String type;
+    private int hazard_lvl;
+    private int priority;
+    
+    public Decision(String t, int hL, int p){
+    }
+    
+    public int getPriority() {
+    }
+    
+    public void execute(ceo CEO){
+    }
 }
