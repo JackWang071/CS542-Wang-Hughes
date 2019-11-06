7c64c108cebc672739632a55c0ad829748b8158f
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-11-05 21:35:01 -0800

    Create Evacuation.java

---
+++ Lab-10/Evacuation.java
@@ -0,0 +1,22 @@
+package Lab10;
+
+/**
+ *
+ * @author Jack and Duran
+ */
+public class Evacuation extends Decision {
+    
+    private String type;
+    private int hazard_lvl;
+    private int priority;
+    
+    public Evacuation(String t, int hL, int p){
+        type = t;
+        hazard_lvl = hL;
+        priority = p;
+    }
+    
+    public void execute(ceo CEO){
+        System.out.println("I want a report on this incident on my desk in the next 2 minutes");
+    } 
+}
