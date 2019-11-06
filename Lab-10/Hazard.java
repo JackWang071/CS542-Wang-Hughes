4b5be7a4b681843d77d4a4ba242e85a78fd13f1c
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-11-05 21:36:34 -0800

    Update Hazard.java

--- Lab-10/Hazard.java
+++ Lab-10/Hazard.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cs542_a10;
+package Lab10;
 
 /**
  *
- * @author Jack
+ * @author Jack and Duran
  */
 public class Hazard {
+    
+    private String type;
+    private int hazard_lvl;
+    
+    public Hazard(String t, int hL){
+        type = t;
+        hazard_lvl = hL;
+    }
+    
+    public int getHazard_lvl(){
+        return hazard_lvl;
+    }
     
 }
