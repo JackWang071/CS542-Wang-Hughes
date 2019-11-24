29003095c7651d304c8c5801aab1f890d4f5a8f5
JackWang071 <35980971+JackWang071@users.noreply.github.com>
2019-11-24 15:56:20 -0800

    Add files via upload

--- Strategy-Game/Unit.java
+++ Strategy-Game/Unit.java
@@ -23,7 +23,14 @@ public abstract class Unit implements GameObject_IF{
     private ObjectIcon icon;
     
     public Unit(String name, Army army, int hp, int attack, int move, int def, int range, int cost){
-        
+        this.unitName = name;
+        this.army = army;
+        this.HP = hp;
+        this.attack = attack;
+        this.defense = def;
+        this.moveDist = move;
+        this.range = range;
+        this.cost = cost;
     }
     
     public int[] getPosition(){
@@ -34,5 +41,49 @@ public abstract class Unit implements GameObject_IF{
     }
     public ObjectIcon getIcon(){
         return icon;
+    }
+    
+    public Army getArmy(){
+        return army;
+    }
+    public String getName(){
+        return unitName;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public int getRange(){
+        return range;
+    }
+    public int getHP(){
+        return HP;
+    }
+    public int getMoveDist(){
+        return moveDist;
+    }
+    public int getCost(){
+        return cost;
+    }
+    
+    public void move(int[] new_position){
+        position = new_position;
+    }
+    
+    public void attack(Unit u){
+        u.changeHP(-attack);
+    }
+    
+    public int changeHP(int amt){
+        if (amt < 0){
+            HP += (amt + defense);
+        }
+        else{
+            HP += amt;
+        }
+        return HP;
+    }
+    
+    public boolean is_destroyed(){
+        return (HP <= 0);
     }
 }
