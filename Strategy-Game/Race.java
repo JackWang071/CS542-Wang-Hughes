7cc3b766369f31da92f6658741a5a53746afc7f6
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 03:30:25 -0800

    Update Race.java
    commented

--- Strategy-Game/Race.java
+++ Strategy-Game/Race.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_project;
 
 /**
  *
  * @author Jack and Duran
  */
+
+//interface
 public interface Race {
     public int getAttackBonus();
     public int getMoveBonus();
