25d4cf1e31ab12d29c0367e6446db21c688062da
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 03:29:02 -0800

    Update Fort.java
    commented

--- Strategy-Game/Fort.java
+++ Strategy-Game/Fort.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_project;
 /**
  *
  * @author Jack and Duran
  */
 public class Fort extends Building implements Cloneable{
-    
+    //variables
     private static final int HP_RECOVERY = 0;
     private static final int ATTACK_BOOST = 0;
     private static final int DEFENSE_BOOST = 10;
     private static final String BUILDING_NAME = "Fort";
     
+    //load in fort icon
     private static final ObjectIcon FORT_ICON = new ObjectIcon("projectgraphics/Fort.png");
     
+    //constructor
     public Fort(){
         super(BUILDING_NAME, FORT_ICON, ATTACK_BOOST, DEFENSE_BOOST, HP_RECOVERY);
     }
