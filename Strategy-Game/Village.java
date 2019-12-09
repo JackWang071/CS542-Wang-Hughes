fd99eb428068f800666fa3589a0d6ac74f74f89b
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 03:28:31 -0800

    Update Village.java
    commented

--- Strategy-Game/Village.java
+++ Strategy-Game/Village.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_project;
 /**
  *
  * @author Jack and Duran
  */
 public class Village extends Building implements Cloneable{
-    
+    //variables
     private static final int HP_RECOVERY = 10;
     private static final int ATTACK_BOOST = 0;
     private static final int DEFENSE_BOOST = 0;
     private static final String BUILDING_NAME = "Village";
     
+    //load in village icon
     private static final ObjectIcon VILLAGE_ICON = new ObjectIcon("projectgraphics/Village.png");
     
+    //constructor
     public Village(){
         super(BUILDING_NAME, VILLAGE_ICON, ATTACK_BOOST, DEFENSE_BOOST, HP_RECOVERY);
     }
