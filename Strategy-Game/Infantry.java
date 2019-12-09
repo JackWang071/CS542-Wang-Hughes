4e4d78c310ab31a795433897a60b6b0f50a22797
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 03:26:43 -0800

    Update Infantry.java
    commented

--- Strategy-Game/Infantry.java
+++ Strategy-Game/Infantry.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_project;
 /**
  *
  * @author Jack and Duran
  */
 public class Infantry extends Unit {
+    //variables
     private static final String UNIT_NAME = "Infantry";
     private static final int BASE_HP = 100;
     private static final int UNIT_COST = 5;
@@ -17,8 +13,10 @@ public class Infantry extends Unit {
     private static final int BASE_ATTACK = 20;
     private static final int BASE_DEFENSE = 8;
     
+    //load in infantry icon
     private static final ObjectIcon INFANTRY_ICON = new ObjectIcon("projectgraphics/Infantry.png");
     
+    //constructor
     public Infantry(Army army){
         super(UNIT_NAME, 
                 army, 
@@ -30,7 +28,8 @@ public class Infantry extends Unit {
                 UNIT_COST,
                 INFANTRY_ICON);
     }
-
+    
+    //attacking function
     public void attack(Unit target){
         if(can_attack()){
             target.changeHP(-(getAttack()));
