4b09e8be5ca9cd0006728b2189f8dfa53eb0dee8
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 17:01:47 -0800

    Update GameObject_IF.java
    commented

--- Strategy-Game/GameObject_IF.java
+++ Strategy-Game/GameObject_IF.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_project;
 
 /**
  *
  * @author Jack and Duran
  */
+
+//interface for GameObject
 public interface GameObject_IF extends Cloneable{
     GridSquare getPosition();
     void setStartingPosition(GridSquare startPos);
