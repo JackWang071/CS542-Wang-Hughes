7081fd0df9dd8a6dececf8c31abcb409cd5994da
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 17:01:18 -0800

    Update GameManager_IF.java
    commented

--- Strategy-Game/GameManager_IF.java
+++ Strategy-Game/GameManager_IF.java
@@ -2,16 +2,12 @@ package cs542_project;
 
 import java.util.List;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Jack and Duran
  */
+
+//interface for GameManager
 public interface GameManager_IF {
     public GameGUI getGUI();
     
