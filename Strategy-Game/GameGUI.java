840bfe1b1e795ec607d5670d519bf29ae6444341
Alexander Hughes <duran.alexander.hughes@gmail.com>
2019-12-09 16:23:33 -0800

    Update GameGUI.java
    commented

--- Strategy-Game/GameGUI.java
+++ Strategy-Game/GameGUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs542_project;
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
@@ -16,12 +11,14 @@ import java.util.List;
  */
 public class GameGUI extends JFrame {
     
+    //variables
     private GameBoard board;
     private GameOptionPanel game_options_panel;
     private GameStartPanel start_panel;
     private ArmySetupPanel army_setup_panel;
     private GameManager_IF game_manager;
     
+    //constructor
     public GameGUI(GameManager_IF game_manager){
         super("Jack and Alex Project");
         this.game_manager = game_manager;
@@ -34,6 +31,7 @@ public class GameGUI extends JFrame {
         game_options_panel = new GameOptionPanel(this);
     }
     
+    //display start panel
     public void showStartPanel(){
         add(start_panel, BorderLayout.CENTER);
         start_panel.chooseStartingOptions();
@@ -41,6 +39,7 @@ public class GameGUI extends JFrame {
         setVisible(true);
     }
     
+    //displays game board
     public void showBoard(int size){
         board.createBoard(size);
         remove(start_panel);
@@ -49,6 +48,7 @@ public class GameGUI extends JFrame {
         repaint();
     }
     
+    //readys army setup panel
     public void showArmySetup(){
         add(army_setup_panel, BorderLayout.EAST);
         army_setup_panel.startArmySetup();
@@ -56,6 +56,7 @@ public class GameGUI extends JFrame {
         repaint();
     }
     
+    //readys board for game to start
     public void showReadyGame(){
         remove(army_setup_panel);
         add(game_options_panel, BorderLayout.EAST);
@@ -69,35 +70,43 @@ public class GameGUI extends JFrame {
         game_manager.getCurrentArmy().updateUnitStatuses();
     }
     
+    //shows starting, moving, or attacking titles
     public void highlightAffectedTiles(Unit actor, String purpose){
         board.highlightAffectedTiles(actor, purpose);
     }
     
+    //show army spawn locations
     public void highlightStartingPositions(Army army){
         board.highlightStartingPositions(army);
     }
     
+    //clears option panel
     public void clearOptionsPanel(){
         game_options_panel.clearOptionsPanel();
     }
     
+    //clears board
     public void clearBoard(){
         board.clearBoard();
     }
     
+    //shows updates GUI to display current title details
     public void updateGUI_selectedTile(GridSquare g){
         clearOptionsPanel();
         game_options_panel.currentTileDetails(g);
     }
     
+    //adds buildings to board
     public void setupBuildings(List<Building> buildings){
         board.addBuildings(buildings);
     }
     
+    //gets game manager
     public GameManager_IF getManager(){
         return game_manager;
     }
     
+    //declare winner and print appropriate message
     public void declareVictory(){
         remove(board);
         remove(game_options_panel);
@@ -108,10 +117,12 @@ public class GameGUI extends JFrame {
         repaint();
     }
     
+    //save game
     public void saveData(SaveData save){
         board.saveBoardDetails(save);
     }
     
+    //load game
     public void loadSaveData(SaveData save){
         showBoard(save.getBoardSize());
         board.loadBoardDetails(save);
